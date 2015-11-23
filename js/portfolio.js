a8aea3e14990dc0918f543db17ea13197b42354c
Rhiannon Argote <rhiannon.argote@gmail.com>
2015-11-23 11:55:04 +1100

    working bubbles

---
+++ js/portfolio.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+	var bubble = function (event) {
+		//console.log(event.clientX, event.clientY);
+
+		var $b = $('<div/>').addClass('bubble');
+		$b.css ({
+			left: event.clientX,
+			top: event.clientY		
+		});
+
+	$('body').append($b);
+
+	// Clean up after ourselves
+	setTimeout(function () {
+		
+		$b.animate({
+			top: -1000 //Disappear of the top of the screen.
+		}, 2000, function () {
+			$b.remove();
+		});
+		
+	}, 1400);
+};
+
+$(window).on('mousemove', bubble);
+
+});
+
